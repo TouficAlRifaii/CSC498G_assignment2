a576fbe83670cad912cfc017240d0d4b0332449e
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-02-16 18:37:27 +0200

    Finishing the dropDown Menu

--- app/src/main/java/com/lau/geeklove/MainActivity.java
+++ app/src/main/java/com/lau/geeklove/MainActivity.java
@@ -3,12 +3,36 @@ package com.lau.geeklove;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String [] languages= {"Java" , "Python" , "SQL" , "Ruby" , "CSS" , "Kotlin"};
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter <String> adapterItem ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+        adapterItem = new ArrayAdapter<String>(this,R.layout.list_item,languages);
+        autoCompleteTxt.setAdapter(adapterItem);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),"Item: " + item , Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
