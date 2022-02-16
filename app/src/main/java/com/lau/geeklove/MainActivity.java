2181d1701b2c57ddd5e03ca120adcdf823d68bff
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-02-16 23:44:05 +0200

    Configuration of images

--- app/src/main/java/com/lau/geeklove/MainActivity.java
+++ app/src/main/java/com/lau/geeklove/MainActivity.java
@@ -1,5 +1,7 @@
 package com.lau.geeklove;
 
+import static android.os.SystemClock.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,12 +9,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String [] languages= {"Java" , "Python" , "Swift" , "Ruby" , "C Sharp" , "Kotlin"};
+    String[] languages = {"Java", "Python", "Swift", "Ruby", "C#", "Kotlin"};
     AutoCompleteTextView autoCompleteTxt;
-    ArrayAdapter <String> adapterItem ;
+    ArrayAdapter<String> adapterItem;
+    ImageView imageView;
 
 
     @Override
@@ -21,17 +25,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         autoCompleteTxt = findViewById(R.id.auto_complete_txt);
-        adapterItem = new ArrayAdapter<String>(this,R.layout.list_item,languages);
+        adapterItem = new ArrayAdapter<String>(this, R.layout.list_item, languages);
         autoCompleteTxt.setAdapter(adapterItem);
+        imageView = findViewById(R.id.imageView);
 
         autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String item = adapterView.getItemAtPosition(i).toString();
-                Language lang = new Language(item);
-                Toast.makeText(getApplicationContext(),"Item: " + lang.getName() , Toast.LENGTH_LONG).show();
+                switch (item) {
+                    case "Java":
+                        imageView.setImageResource(R.drawable.java);
+                        break;
+                    case "Python":
+                        imageView.setImageResource(R.drawable.python);
+                        break;
+                    case "Swift":
+                        imageView.setImageResource(R.drawable.swift);
+                        break;
+                    case "C#":
+                        imageView.setImageResource(R.drawable.csharp);
+                        break;
+                    case "Ruby":
+                        imageView.setImageResource(R.drawable.ruby);
+                        break;
+                    case "Kotlin":
+                        imageView.setImageResource(R.drawable.kotlin);
+                        break;
+                }
+                imageView.setVisibility(ImageView.VISIBLE);
+                Toast.makeText(getApplicationContext(), "Item: " + item, Toast.LENGTH_LONG).show();
             }
         });
 
