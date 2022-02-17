5e56ffccb66a8726f80af1f606e75605e503f783
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-02-17 10:29:02 +0200

    removing useless Comments

--- app/src/main/java/com/lau/geeklove/MainActivity.java
+++ app/src/main/java/com/lau/geeklove/MainActivity.java
@@ -31,68 +31,8 @@ public class MainActivity extends AppCompatActivity {
     EditText nameInput;
     Button button;
     String itemName;
-//    TextView r1col0;
-//    TextView r1col1;
-//    TextView r1col2;
-//    TextView r2col0;
-//    TextView r2col1;
-//    TextView r2col2;
-//    TextView r3col0;
-//    TextView r3col1;
-//    TextView r3col2;
-//    TextView r4col0;
-//    TextView r4col1;
-//    TextView r4col2;
-//    TextView r5col0;
-//    TextView r5col1;
-//    TextView r5col2;
-//    TextView r6col0;
-//    TextView r6col1;
-//    TextView r6col2;
-    boolean[] isFilled;
-
     TableLayout table;
-    ArrayList<String[]> forTable;
 
-//    public void clearTable() {
-//        r1col0 = findViewById(R.id.r1col0);
-//        r1col1 = findViewById(R.id.r1col1);
-//        r1col2 = findViewById(R.id.r1col2);
-//        r2col0 = findViewById(R.id.r2col0);
-//        r2col1 = findViewById(R.id.r2col1);
-//        r2col2 = findViewById(R.id.r2col2);
-//        r3col0 = findViewById(R.id.r3col0);
-//        r3col1 = findViewById(R.id.r3col1);
-//        r3col2 = findViewById(R.id.r3col2);
-//        r4col0 = findViewById(R.id.r4col0);
-//        r4col1 = findViewById(R.id.r4col1);
-//        r4col2 = findViewById(R.id.r4col2);
-//        r5col0 = findViewById(R.id.r5col0);
-//        r5col1 = findViewById(R.id.r5col1);
-//        r5col2 = findViewById(R.id.r5col2);
-//        r6col0 = findViewById(R.id.r6col0);
-//        r6col1 = findViewById(R.id.r6col1);
-//        r6col2 = findViewById(R.id.r6col2);
-//        r1col0.setText("");
-//        r1col1.setText("");
-//        r1col2.setText("");
-//        r2col0.setText("");
-//        r2col1.setText("");
-//        r2col2.setText("");
-//        r3col0.setText("");
-//        r3col1.setText("");
-//        r3col2.setText("");
-//        r4col0.setText("");
-//        r4col1.setText("");
-//        r4col2.setText("");
-//        r5col0.setText("");
-//        r5col1.setText("");
-//        r5col2.setText("");
-//        r6col0.setText("");
-//        r6col1.setText("");
-//        r6col2.setText("");
-//
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,31 +47,6 @@ public class MainActivity extends AppCompatActivity {
         nameInput = (EditText) findViewById(R.id.nameInput);
         button = findViewById(R.id.button);
         table = (TableLayout) findViewById(R.id.table);
-        forTable = new ArrayList<String[]>();
-//        r1col0 = findViewById(R.id.r1col0);
-//        r1col1 = findViewById(R.id.r1col1);
-//        r1col2 = findViewById(R.id.r1col2);
-//        r2col0 = findViewById(R.id.r2col0);
-//        r2col1 = findViewById(R.id.r2col1);
-//        r2col2 = findViewById(R.id.r2col2);
-//        r3col0 = findViewById(R.id.r3col0);
-//        r3col1 = findViewById(R.id.r3col1);
-//        r3col2 = findViewById(R.id.r3col2);
-//        r4col0 = findViewById(R.id.r4col0);
-//        r4col1 = findViewById(R.id.r4col1);
-//        r4col2 = findViewById(R.id.r4col2);
-//        r5col0 = findViewById(R.id.r5col0);
-//        r5col1 = findViewById(R.id.r5col1);
-//        r5col2 = findViewById(R.id.r5col2);
-//        r6col0 = findViewById(R.id.r6col0);
-//        r6col1 = findViewById(R.id.r6col1);
-//        r6col2 = findViewById(R.id.r6col2);
-        isFilled = new boolean[6];
-        for (int i = 0; i < isFilled.length; i++) {
-            isFilled[i] = false;
-        }
-
-
         autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -179,15 +94,6 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Random rand = new Random();
                 int random = rand.nextInt(101);
-//                if (isFilled[isFilled.length - 1]) {
-//                    Toast.makeText(getApplicationContext(), "The table will be cleared", Toast.LENGTH_LONG).show();
-////                    clearTable();
-//                }
-//                if (!isFilled[0]) {
-//                    r1col0.setText(name);
-//                    r1col1.setText(itemName);
-//                    r1col2.setText(random);
-//                }
                 TableRow row = new TableRow(this);
                 row.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.MATCH_PARENT));
                 TextView col0 = new TextView(this);
