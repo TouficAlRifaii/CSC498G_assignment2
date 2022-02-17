63b1b9c8ede3371b4a640e22f4cc53c3e50af6cc
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-02-17 08:44:59 +0200

    Adding the table

--- app/src/main/java/com/lau/geeklove/MainActivity.java
+++ app/src/main/java/com/lau/geeklove/MainActivity.java
@@ -2,18 +2,24 @@ package com.lau.geeklove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     String itemName;
     TextView txtView;
+    TableLayout table;
+    ArrayList<String[]> forTable;
 
 
     @Override
@@ -39,7 +47,9 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         nameInput = (EditText) findViewById(R.id.nameInput);
         button = findViewById(R.id.button);
-        txtView = findViewById(R.id.textView);
+
+        table = (TableLayout) findViewById(R.id.table);
+        forTable = new ArrayList<String[]>();
 
         autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -88,7 +98,25 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Random rand = new Random();
                 int random = rand.nextInt(101);
-                txtView.setText("The relationship between " + name + " and " + itemName + " is based on " + random + "% true love!");
+                String[] toAdd= {name , itemName , String.valueOf(random)};
+                forTable.add(0 , toAdd);
+                TextView col0 = new TextView(this);
+                col0.setText(forTable.get(0)[0]);
+                col0.setTextColor(Color.BLACK);
+                ViewGroup.LayoutParams paramsCol0 = col0.getLayoutParams();
+                col0.setPadding(14 ,14 , 14 ,14);
+                TextView col1 = new TextView(this);
+                col1.setText(forTable.get(0)[1]);
+                TextView col2 = new TextView(this);
+                col2.setText(forTable.get(0)[2]);
+                TableRow row = new TableRow(this);
+                ViewGroup.LayoutParams params = row.getLayoutParams();
+                row.addView(col0);
+                row.addView(col1);
+                row.addView(col2);
+                table.addView(row);
+
+
             }
         }
     }
